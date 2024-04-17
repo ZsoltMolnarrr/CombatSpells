2c02605c210d5cdac0e408ea36646497ccc436c0
Exopandora <mkonrad98@outlook.com>
2024-04-17 18:43:19 +0200

    Migrate Shoulder Surfing API

--- common/src/main/java/net/spell_engine/client/compatibility/ShoulderSurfingCompatibility.java
+++ common/src/main/java/net/spell_engine/client/compatibility/ShoulderSurfingCompatibility.java
@@ -1,7 +1,7 @@
 package net.spell_engine.client.compatibility;
 
-import com.teamderpy.shouldersurfing.api.IShoulderSurfingPlugin;
-import com.teamderpy.shouldersurfing.api.IShoulderSurfingRegistrar;
+import com.github.exopandora.shouldersurfing.api.IShoulderSurfingPlugin;
+import com.github.exopandora.shouldersurfing.api.IShoulderSurfingRegistrar;
 import net.spell_engine.internals.SpellContainerHelper;
 
 public class ShoulderSurfingCompatibility implements IShoulderSurfingPlugin {
