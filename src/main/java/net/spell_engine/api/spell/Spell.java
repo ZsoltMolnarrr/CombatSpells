2c9f546fbb51f47462c7b6226c9a1c1dcb6ce81b
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-09-04 23:08:36 +0200

    Rewrite applying projectile perks

--- src/main/java/net/spell_engine/api/spell/Spell.java
+++ src/main/java/net/spell_engine/api/spell/Spell.java
@@ -23,7 +23,10 @@ public class Spell {
         public int level_requirement_per_tier = 10;
     }
 
-    public enum Mode { CAST, ITEM_USE }
+    public enum Mode {
+        CAST,
+        ITEM_USE
+    }
     public Mode mode = Mode.CAST;
 
     public Cast cast = new Cast();
---
+++ src/main/java/net/spell_engine/mixin/arrow/RangedWeaponItemMixin.java
@@ -0,0 +1,42 @@
+package net.spell_engine.mixin.arrow;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.RangedWeaponItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+import net.spell_engine.api.spell.Spell;
+import net.spell_engine.api.spell.SpellInfo;
+import net.spell_engine.internals.SpellContainerHelper;
+import net.spell_engine.internals.SpellRegistry;
+import net.spell_engine.internals.arrow.ArrowExtension;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(RangedWeaponItem.class)
+public class RangedWeaponItemMixin {
+
+    @WrapOperation(method = "shootAll", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/RangedWeaponItem;createArrowEntity(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;Z)Lnet/minecraft/entity/projectile/ProjectileEntity;"))
+    private ProjectileEntity shootAll_wrap_createArrowEntity(
+            RangedWeaponItem instance, World world, LivingEntity shooter, ItemStack weaponStack, ItemStack projectileStack, boolean critical,
+            Operation<ProjectileEntity> original) {
+        var projectile = original.call(instance, world, shooter, weaponStack, projectileStack, critical);
+        if (shooter instanceof PlayerEntity player
+                && projectile instanceof ArrowExtension arrow) {
+            var container = SpellContainerHelper.getEquipped(weaponStack, player);
+            for (var idString : container.spell_ids()) {
+                var id = Identifier.of(idString);
+                var spell = SpellRegistry.getSpell(id);
+                if (spell != null && spell.mode == Spell.Mode.ITEM_USE && spell.arrow_perks != null) {
+                    var info = new SpellInfo(spell, id);
+                    arrow.applyArrowPerks(info);
+                }
+            }
+        }
+        return projectile;
+    }
+}
