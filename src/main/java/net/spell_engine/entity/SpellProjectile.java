b8b11579b35590fa064771ee4cae5912f3d1bcb9
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2024-09-18 21:40:52 +0200

    Cherry pick: Improve ricochet stability Commit: 214743ac730b

--- src/main/java/net/spell_engine/entity/SpellProjectile.java
+++ src/main/java/net/spell_engine/entity/SpellProjectile.java
@@ -446,6 +446,7 @@ public class SpellProjectile extends ProjectileEntity implements FlyingSpellEnti
 
         // Set trajectory
         var newTarget = otherTargets.get(0);
+        this.setPosition(target.getPos().add(0, target.getHeight() * 0.5F, 0));
         this.setFollowedTarget(newTarget);
 
         var distanceVector = (newTarget.getPos().add(0, newTarget.getHeight() / 2F, 0))
